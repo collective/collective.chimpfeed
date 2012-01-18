b7fafe97cee9668045270b5081821a7990fb350b
Malthe Borch <mborch@gmail.com>
2012-01-18 13:06:12 +0100

    Preparing release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*pathnames):
     return open(os.path.join(os.path.dirname(__file__), *pathnames)).read()
 
-version = '1.0-beta4'
+version = '1.0'
 
 setup(name='collective.chimpfeed',
       version=version,
