17960fabf0de127cfc755550821a2f206bbab5d8
Malthe Borch <mborch@gmail.com>
2012-10-29 12:58:32 +0100

    Preparing release.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,8 @@
 Changes
 =======
 
-In next release ...
+1.8.2 (2012-10-29)
+------------------
 
 Features:
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*pathnames):
     return open(os.path.join(os.path.dirname(__file__), *pathnames)).read()
 
-version = '1.8.1'
+version = '1.8.2'
 
 setup(name='collective.chimpfeed',
       version=version,
